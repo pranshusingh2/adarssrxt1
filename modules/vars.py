37f0073d631b97ad33045e4a414f27945ff0e228
Adarsh Rathaur <141756805+mradarshr@users.noreply.github.com>
2024-06-07 17:41:45 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "20484128"
 api_hash = "22d633560d88a98c871c4e7b13ff4261"
-bot_token = "7444415767:AAHkJaD40H9B7_hUjwXN2tmf6zQGg4C5gPs"
+bot_token = "7077659432:AAE3bR1xdY8cjLMDwBPDBILttYkXHpSgYxA"
