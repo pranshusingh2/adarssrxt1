bdaadd9b4318abf49f8b58a56b4b5f2ab0e6e357
Adarsh Rathaur <141756805+mradarshr@users.noreply.github.com>
2024-06-07 18:14:21 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -200,7 +200,7 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
         if thumb == "no":
             thumbnail = f"{filename}.jpg"
         else:
-            thumbnail = "thumb.jpg"
+            thumbnail = thumb
     except Exception as e:
         await m.reply_text(str(e))
 
